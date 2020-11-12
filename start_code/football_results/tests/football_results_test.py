cc531ef993bbf3c7215e8b7599eebd9cb7189335
Laura Dobie <moving.toyshop@gmail.com>
2020-11-12 17:21:12 +0000

    Put test result dictionaries in the test set up into a list called final_scores.

--- start_code/football_results/tests/football_results_test.py
+++ start_code/football_results/tests/football_results_test.py
@@ -3,31 +3,31 @@ from src.football_results import *
 
 class FootballResultsTest(unittest.TestCase):
     def setUp(self):
-        self.result_1 = {    
-        "home_score": 1,
-        "away_score": 0
-    }
-
-        self.result_2 = {    
-        "home_score": 0,
-        "away_score": 1
-    }
-
-        self.result_3 = {    
-        "home_score": 2,
-        "away_score": 2
-    }
+        self.final_scores = [
+            {    
+                "home_score": 1,
+                "away_score": 0
+            },
+            {    
+                "home_score": 0,
+                "away_score": 1
+            },
+            {    
+                "home_score": 2,
+                "away_score": 2
+            }
+        ]
 
     # Test we get the right result string for a final score dictionary representing -
     def test_get_result(self):
     # Home win
-        home_win = get_result(self.result_1)
+        home_win = get_result(self.final_scores[0])
         self.assertEqual("Home win", home_win)
         # Away win
-        away_win = get_result(self.result_2)
+        away_win = get_result(self.final_scores[1])
         self.assertEqual("Away win", away_win)
         # Draw
-        draw = get_result(self.result_3)
+        draw = get_result(self.final_scores[2])
         self.assertEqual("Draw", draw)
 
     # Test we get right list of result strings for a list of final score dictionaries. 
